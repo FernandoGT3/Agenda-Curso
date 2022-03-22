655f2e5371aed03723d4577b9316171e9c343a3b
Luiz Moloni <luiz.moloni@gmail.com>
2022-03-22 12:06:00 -0300

    Criando a Página de Login/Cadastro
    Criamos o Controller da Página de Login, para que esta seja renderizada na rota /login/index', e no Arquivo login.ejs, criamos a página, já com csrfToken para segurança do formulário

--- routes.js
+++ routes.js
@@ -3,10 +3,12 @@ const express = require('express');
 const route = express.Router();
 
 const homeController = require('./src/controllers/homeController'); 
+const loginController = require('./src/controllers/loginController');
 
-//A Rota vai decidir qual controller vai ser utilizado
-//Controller -> Contola a Rota, decide as coisas sobre a rota ->> o model(controlar os dados), e o view(o que será renderizado)
+
 //Rotas da Home
-route.get("/", homeController.paginaInicial);
+route.get("/", homeController.index);
 
+//Rotas de Login
+route.get('/login/index', loginController.index);
 module.exports = route;
